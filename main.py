514c8d9b98cee8ec68952e34d56fecf7dfad997c
het28052001 <het.shah@fxis.ai>
2024-08-09 12:40:41 +0530

    Automated changes based on user prompt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from patterns_print import *
+from create import print_reverse_star_pyramid
 
 print("hello World!")
 printFirstTenNum()
 print()
 printStars(5)
+print_reverse_star_pyramid(10)
--- patterns_print.py
+++ patterns_print.py
@@ -7,4 +7,9 @@ def printStars(n):
         for j in range(i):
             print("*", end=" ")
         print()
-    
+
+def print_reverse_star_pyramid(n):
+    for i in range(n, 0, -1):
+        print(' ' * (n - i) + '*' * (2 * i - 1))
+
+print_reverse_star_pyramid(10)
